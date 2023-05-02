d280ca5624529222b5da77f861bf06347c97e543
fabellaivan <ivano.fabella@gmail.com>
2023-05-02 19:00:10 -0300

    Udemy
    RPG y Angular

---
+++ Angular/01-typescript/src/topics/01-basic-types.ts
@@ -0,0 +1,17 @@
+let name = 'Ivan';
+const name2 = 'Ivano';
+let hpPoints: number | string;
+let hpPoints2: number | "full";
+const isAlive: boolean = true;
+hpPoints = "hola mundo";
+hpPoints = 10;
+hpPoints2 = 12;
+
+console.log({
+    name,
+    name2,
+    hpPoints,
+    hpPoints2,
+    isAlive
+})
+export{};
---
+++ Angular/primeraPrueba.ts
@@ -0,0 +1,3 @@
+console.log("Hola mundo");
+const a: number = 100;
+console.log(a);
